141e5b188fe0a7b3240ff2a8de8c215d6c31f756
Bogdan Polak <bogdanpol@gmail.com>
2020-12-29 18:11:10 +0100

    _updated style

--- Part01/04/Ext-lite.js
+++ Part01/04/Ext-lite.js
@@ -16,46 +16,58 @@ Ext.create = function(item) {
 };
 
 function ComponentBuilder() {
+	
 	return {
 		buildExtGrid: function(item) {
+
 			var grid = document.createElement("div");
 			grid.setAttribute("id", "data-grid1");
-			grid.setAttribute("class", "db-grid");
+			grid.setAttribute("class", "datagrid-div");
+
 			var title = document.createElement("div");
 			title.setAttribute("class", "gridTitle");
 			title.innerHTML = item.title;
-			var table = document.createElement("table");
-			table.setAttribute("class", "dataTable");
-			var header = table.createTHead();
-			var htmltableHeaderRow = header.insertRow(0);   
-			for (i=0;i<item.columns.length;i++) {
-				th = document.createElement('th');
-				th.innerHTML = item.columns[i].text;
-				htmltableHeaderRow.appendChild(th);
-			}
-			var body = table.createTBody();
-			const addGridRow = function(rowidx) {
-				const htmltableRow = body.insertRow(i);
-				const rowObj = {};
-				for (colIdx=0; colIdx<item.columns.length; colIdx++) {
-					prop = item.columns[colIdx].dataIndex;
-					const value = item.data[rowidx][prop];
-					rowObj[prop] = value;
-					td = document.createElement('td');
-					td.innerHTML = value;
-					htmltableRow.appendChild(td);
-				}
-				// var record=[]; record[0] = {};  record[0].data = o;
-				htmltableRow.onclick = function() {
-					return item.listeners.select(htmltableRow,rowObj)
-				};  
-			};
-			for (var i=0;i<item.data.length;i++) {
-				addGridRow(i);
-			};
 			grid.appendChild(title);
+
+			var table = document.createElement("table");
+			table.setAttribute("class", "datagrid-table");
 			grid.appendChild(table);
+
+			buildDataTableHeader(table);
+			buildDataTableBody(table);
 			return grid;
+
+			function buildDataTableHeader(table){
+				var header = table.createTHead();
+				var htmltableHeaderRow = header.insertRow(0);   
+				for (i=0;i<item.columns.length;i++) {
+					th = document.createElement('th');
+					th.innerHTML = item.columns[i].text;
+					htmltableHeaderRow.appendChild(th);
+				}
+			}
+			function buildDataTableBody(table){
+				const body = table.createTBody();
+				const addGridRow = function(rowidx) {
+					const htmltableRow = body.insertRow(i);
+					const rowObj = {};
+					for (colIdx=0; colIdx<item.columns.length; colIdx++) {
+						prop = item.columns[colIdx].dataIndex;
+						const value = item.data[rowidx][prop];
+						rowObj[prop] = value;
+						td = document.createElement('td');
+						td.innerHTML = value;
+						htmltableRow.appendChild(td);
+					}
+					// var record=[]; record[0] = {};  record[0].data = o;
+					htmltableRow.onclick = function() {
+						return item.listeners.select(htmltableRow,rowObj)
+					};  
+				};
+				for (var i=0;i<item.data.length;i++) {
+					addGridRow(i);
+				};
+			}
 		}
 	}	
 }
